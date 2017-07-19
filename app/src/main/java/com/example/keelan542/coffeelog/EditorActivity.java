c07f1154130d9ca29a226c2501537fd94f1aee69
Keelan Byrne <keelanb542@gmail.com>
2017-07-19 19:14:40 +0100

    Add intent from MainActivity to EditorActivity and add app bar menu to Editor(no functionality yet)

--- app/src/main/java/com/example/keelan542/coffeelog/EditorActivity.java
+++ app/src/main/java/com/example/keelan542/coffeelog/EditorActivity.java
@@ -1,7 +1,8 @@
 package com.example.keelan542.coffeelog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/keelan542/coffeelog/MainActivity.java
+++ app/src/main/java/com/example/keelan542/coffeelog/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.keelan542.coffeelog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup FAB to open editor activity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.FAB);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

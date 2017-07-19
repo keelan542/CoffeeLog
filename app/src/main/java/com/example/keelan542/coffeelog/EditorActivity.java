5415307398303f286e2bfc6841c0132da9d84cfb
Keelan Byrne <keelanb542@gmail.com>
2017-07-19 20:01:03 +0100

    Get references to edit fields andn add up button to app bar options

--- app/src/main/java/com/example/keelan542/coffeelog/EditorActivity.java
+++ app/src/main/java/com/example/keelan542/coffeelog/EditorActivity.java
@@ -1,12 +1,15 @@
 package com.example.keelan542.coffeelog;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.keelan542.coffeelog.data.CoffeeContract.CoffeeEntry;
@@ -25,6 +28,18 @@ public class EditorActivity extends AppCompatActivity {
     // Method value
     private String mMethod;
 
+    // Coffee used edit text
+    private EditText mCoffeeUsedEditText;
+
+    // Yield edit text
+    private EditText mYieldEditText;
+
+    // Minutes edit text
+    private EditText mMinutesEditText;
+
+    // Seconds edit text
+    private EditText mSecondsEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +47,10 @@ public class EditorActivity extends AppCompatActivity {
 
         mMethodSpinner = (Spinner) findViewById(R.id.method_spinner);
         mExtractionSpinner = (Spinner) findViewById(R.id.extraction_spinner);
+        mCoffeeUsedEditText = (EditText) findViewById(R.id.coffee_used_edit_text);
+        mYieldEditText = (EditText) findViewById(R.id.water_used_edit_text);
+        mMinutesEditText = (EditText) findViewById(R.id.time_mins_edit_text);
+        mSecondsEditText = (EditText) findViewById(R.id.time_sec_edit_text);
 
         setupSpinners();
     }
@@ -41,6 +60,26 @@ public class EditorActivity extends AppCompatActivity {
         // Inflate menu
         getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar
+        switch (item.getItemId()) {
+            // Respond to a click on the save button
+            case R.id.action_save:
+                saveEntry();
+                finish();
+                return true;
+            // Respond to a click on the delete button
+            case R.id.action_delete:
+                return true;
+            // Respond to a click on up arrow
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void setupSpinners() {
@@ -95,5 +134,9 @@ public class EditorActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void saveEntry() {
+
     }
 }

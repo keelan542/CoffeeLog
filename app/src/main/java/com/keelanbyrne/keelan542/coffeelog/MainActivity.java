4107bddeb6eae4c6afe408890e9dfd10bbdfc1c3
Keelan Byrne <keelanb542@gmail.com>
2017-08-16 18:27:50 +0100

    Hook up RecyclerView in MainActivity

--- app/src/main/java/com/keelanbyrne/keelan542/coffeelog/MainActivity.java
+++ app/src/main/java/com/keelanbyrne/keelan542/coffeelog/MainActivity.java
@@ -1,7 +1,6 @@
 package com.keelanbyrne.keelan542.coffeelog;
 
 import android.app.LoaderManager;
-import android.content.ContentUris;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
@@ -10,9 +9,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.keelanbyrne.keelan542.coffeelog.data.CoffeeContract.CoffeeEntry;
@@ -21,6 +20,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     // Loader id
     private static final int LOADER_ID = 1;
+
+    // RecylerView
+    private RecyclerView mRecyclerView;
+
+    // RecyclerView adapter
+    private CoffeeRecyclerAdapter mRecyclerAdapter;
 
     // Cursor adapter
     private CoffeeCursorAdapter mAdapter;
@@ -49,16 +54,21 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // Get reference to emtpy view
         mEmptyView = (TextView) findViewById(R.id.empty_view);
 
+        // Find RecyclerView and set layout manager
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
         // Find listView and set empty view
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setEmptyView(mEmptyView);
+        //ListView listView = (ListView) findViewById(R.id.list);
+        //listView.setEmptyView(mEmptyView);
 
         // Create instance of CoffeeCursorAdapter
-        mAdapter = new CoffeeCursorAdapter(this, null);
+       // mAdapter = new CoffeeCursorAdapter(this, null);
 
         // Set adapter on listView
-        listView.setAdapter(mAdapter);
+        //listView.setAdapter(mAdapter);
 
+        /*
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -75,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 startActivity(intent);
             }
         });
+        */
 
         // Initialise loader
         getLoaderManager().initLoader(LOADER_ID, null, this);
@@ -105,14 +116,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        // Swap the new cursor in
-        mAdapter.swapCursor(data);
+        // Create instance of CoffeeRecyclerAdapter
+        mRecyclerAdapter = new CoffeeRecyclerAdapter(this, data);
+
+        // Set adapter on recycler view
+        mRecyclerView.setAdapter(mRecyclerAdapter);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         // Called when last Cursor provided to onLoadFinished()
         // is about to be closed
-        mAdapter.swapCursor(null);
+        //mAdapter.swapCursor(null);
     }
 }

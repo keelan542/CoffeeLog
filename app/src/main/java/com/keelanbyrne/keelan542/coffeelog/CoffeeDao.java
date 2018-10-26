c74e8fac7276eef5d2f1cfbe5d5f34ebe9534f15
Keelan Byrne <keelanb542@gmail.com>
2018-10-26 18:29:06 +0100

    Fix error in DAO class

--- app/src/main/java/com/keelanbyrne/keelan542/coffeelog/CoffeeDao.java
+++ app/src/main/java/com/keelanbyrne/keelan542/coffeelog/CoffeeDao.java
@@ -1,6 +1,7 @@
 package com.keelanbyrne.keelan542.coffeelog;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -8,6 +9,7 @@ import android.arch.persistence.room.Update;
 
 import java.util.List;
 
+@Dao
 public interface CoffeeDao {
 
     @Insert
